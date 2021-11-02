0a33146e10cb3cd1b3c3e17a460e3945a42f2dd0
tuc289 <62360632+tuc289@users.noreply.github.com>
2021-11-02 11:30:04 -0400

    Create Sanger_to_blast.sh
    pre-requisite : Tracy (0.6.1 or higher), blast+ [both available in bioconda]

---
+++ Scripts/Sanger_to_blast.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+cd $1
+
+ls *.ab1 > input.txt
+while read oldname; do
+  newname=$(awk -F'_' '{ print $2 "_" $3 ".ab1" }' <<< ${oldname} | \
+        awk '{ print $1 }');
+  cp ${oldname} ${newname};
+done<input.txt
+
+for f in *_ITS1.ab1
+do 
+tracy assemble $f ${f%_ITS1.ab1}_ITS4.ab1 -o ${f%_ITS1.ab1}
+rm $f
+rm ${f%_ITS1.ab1}_ITS4.ab1
+rm input.txt
+mv ${f%_ITS1.ab1}.cons.fa ${f%_ITS1.ab1}.concensus.fasta
+done
+
+rm *.json
+rm *.vertical
+rm *.align.fa
+
+for t in *.concensus.fasta
+do
+blastn -db nr -query $t -remote -out ${t%.concensus.fasta}.blast.txt
+done
+
+
+
+for f in *.txt
+do; echo $f; sed -n '20,40p' $f; echo "\n";
+done > blast_results_combined.txt
