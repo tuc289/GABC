22a5718b2f002d80ec941e2ffd3531de6dac556f
tuc289 <tuc289@psu.edu>
2022-02-22 12:56:30 -0500

    create pima_assembly_time.sh

---
+++ Scripts/pima_assembly_time.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Purpose of this script is to check the time of initial process of PIMA
+#Usage: pima_time.sh <input directory as barcode##, with all the fast5 file>
+
+export PATH=$PATH:/gpfs/group/jzk303/default/data/CDC_bacilli/programs/ont-guppy-cpu/bin
+
+
+echo "Guppy basecalling starts at $(date)" > guppy_start_time
+guppy_basecaller -i $1 -r -s $1 --num_callers 6 --cpu_threads_per_caller 4 -c dna_r9.4.1_450bps_hac.cfg 
+
+echo "Merge fastq file into one"
+echo "Guppy basecalling ends at $(date)" > guppy_finish_time
+cd $1 
+cat *.fastq > combined.fastq
+
+echo "qcat quality check starts at $(date)" > ../qcat_start_time
+
+qcat --guppy --min-score 65 --kit RBK004 -f combined.fastq -b $1 
+
+echo "qcat qualit check ends at $(date)" > ../qcat_end_time
+
+echo "Assembly reads starts at $(data)" > ../flye_start_time
+export PATH=$PATH:/gpfs/group/jzk303/default/data/CDC_bacilli/programs/Flye/bin
+
+flye --nano-hq barcode12.fastq --genome-size 5m -o ./ -t 20
+
+echo "Assembly reads ends at $(date)" > ../flye_end_time
+
+echo "Assembly polished starts at $(date)" > ../minimap2_start_time
+
+minimap2 -x map-ont -t 20 assembly.fasta barcode12.fastq > temporary_path
+racon -t 20 barcode12.fastq temporary_path assembly.fasta > polished_contigs.fasta
+
+echo "Assembly polishing ends at $(date)" > ../minimap2_end_time
+
+cat guppy_start_time guppy_finish_time qcat_start_time qcat_end_time flye_start_time flye_end_time minimap2_start_time minimap2_end_time > time_log
+
+
+
+
