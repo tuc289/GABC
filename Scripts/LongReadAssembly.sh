c72ad861db8f22d7dc2f46d19c05eb3f95d4ffe7
Taejung Chung <62360632+tuc289@users.noreply.github.com>
2022-03-03 12:16:53 -0500

    Update LongReadAssembly.sh

--- Scripts/LongReadAssembly.sh
+++ Scripts/LongReadAssembly.sh
@@ -4,18 +4,18 @@
 #Usage qsub -F "<input directory>" LongReadAssembly.sh
 
 #PBS -A jzk303_d_g_gc_default
-#PBS -l nodes=1:ppn=??:gpus=1 
-#PBS -l walltime=??:??:??
-#PBS -l pmem=??
+#PBS -l nodes=1:ppn=24:gpus=1 
+#PBS -l walltime=5:00:00
+#PBS -l pmem=32gb
 
 conda activate <your directory with program installed>
 export PATH=$PATH:/gpfs/group/jzk303/default/data/CDC_bacilli/programs/ont-guppy/bin
-export PATH=/usr/local/cuda-9.1/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-9.1/lib64:$LD_LIBRARY_PATH
+export PATH=/usr/local/cuda-10.2/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64:$LD_LIBRARY_PATH
 
 
 echo "Guppy basecalling starts at $(date)" > guppy_start_time
-guppy_basecaller -i $1 -r -s -$1 --num_caller --num_callers 14 --gpu_runners_per_device 8 --x "cuda:0" --flowcell FLO-MIN106 --kit SQK-RBK004
+guppy_basecaller -i $1 -r -s -$1 --num_callers 6 --gpu_runners_per_device 4 -x "cuda:0" --flowcell FLO-MIN106 --kit SQK-RBK004
 
 
 ## needs to be tested in 3/3/2022, then extend it to the other programs (i.e., qcat, flye, and so on...)
